f9c59f3e8053b72bd5265aa896a7de76c0206cf8
luanvo1910 <vothanhluan19102001@gmail.com>
2024-10-29 12:05:40 +0700

    excercise 5 and 6

--- 3_longestSubstring.js
+++ 3_longestSubstring.js
@@ -2,7 +2,6 @@
 // Given a string s, find the length of the longest substring without repeating characters.
 
 // Example:
-
 // Input: s = "abcabcbb"
 // Output: 3
 // Explanation: The answer is "abc", with the length of 3.
